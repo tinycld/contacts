03a6afad01d0832774e345620ef6242d6d241344
Nathan Stitt <nathan@stitt.org>
2026-09-15 11:13:33 -0500

    chore(contacts): require @tinycld/core >=0.1.0

--- manifest.ts
+++ manifest.ts
@@ -66,7 +66,7 @@ const manifest = {
         },
     },
     repository: { url: 'https://github.com/tinycld/contacts' },
-    peerVersions: { '@tinycld/core': '>=0.0.6 <0.1.0' },
+    peerVersions: { '@tinycld/core': '>=0.1.0 <0.2.0' },
 }
 
 export default manifest
